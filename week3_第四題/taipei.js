9abe20691c2d4cb91bd3d03940472c8a2719ffa8
GAP <90244749+ziven8807@users.noreply.github.com>
2025-01-26 23:25:14 +0800

    Update taipei.js

--- week3_第四題/taipei.js
+++ week3_第四題/taipei.js
@@ -1,369 +1,105 @@
-const data = [
-  {
-    name: "新北投溫泉區",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000848.jpg",
-  },
-  {
-    name: "大稻埕碼頭",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000340.jpg",
-  },
-  {
-    name: "士林官邸",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D7/E150/F719/71eb4b56-f771-43bc-856c-2fb265a5cc6e.jpg",
-  },
-  {
-    name: "國立故宮博物院",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D14/E810/F21/48d66fbd-1ba3-4efd-837a-3767db5f52e0.jpg",
-  },
-  {
-    name: "北投圖書館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D878/E726/F304/2d6e8b50-76ce-4b7e-a795-0357cd6f2b3e.jpg",
-  },
-  {
-    name: "陽明山溫泉區",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000985.jpg",
-  },
-  {
-    name: "關渡、金色水岸、八里左岸自行車道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000979.jpg",
-  },
-  {
-    name: "大安森林公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D11/E257/F345/546f42a1-cbfb-49fc-b4d8-c33f1c44190c.jpg",
-  },
-  {
-    name: "地熱谷",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11003993.jpg",
-  },
-  {
-    name: "南港山系-象山親山步道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11001481.jpg",
-  },
-  {
-    name: "艋舺龍山寺",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D226/E920/F665/1f26af86-e907-44c9-b4d7-5c498dacfc6c.jpg",
-  },
-  {
-    name: "行天宮",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D509/E14/F983/d5f49ce0-139c-4e8b-81cf-39e2b32e79bf.jpg",
-  },
-  {
-    name: "梅庭",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D931/E93/F956/374f6b31-840b-407f-82b7-e6f70d4d0c49.JPG",
-  },
-  {
-    name: "中正紀念堂",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000375.jpg",
-  },
-  {
-    name: "台北探索館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D6/E578/F270/f6f669d9-997f-48c0-845a-be267b3d6ad5.jpg",
-  },
-  {
-    name: "二格山系-指南宮貓空親山步道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11002820.jpg",
-  },
-  {
-    name: "北投溫泉博物館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000382.jpg",
-  },
-  {
-    name: "琉園水晶博物館(暫時休館)",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000813.jpg",
-  },
-  {
-    name: "華山1914文化創意產業園區",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000956.jpg",
-  },
-  {
-    name: "長榮海事博物館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11002524.jpg",
-  },
-  {
-    name: "台北101",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000361.jpg",
-  },
-  {
-    name: "袖珍博物館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000762.jpg",
-  },
-  {
-    name: "新店溪、大漢溪與淡水河自行車道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000980.jpg",
-  },
-  {
-    name: "北投公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D0/E196/F761/d15e55ea-c2a2-41cf-a931-42adab2bb739.jpg",
-  },
-  {
-    name: "台北霞海城隍廟",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000930.jpg",
-  },
-  {
-    name: "國父紀念館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000358.jpg",
-  },
-  {
-    name: "陽明山國家公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11003994.JPG",
-  },
-  {
-    name: "臺北市立動物園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11003997.JPG",
-  },
-  {
-    name: "碧湖公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11002936.jpg",
-  },
-  {
-    name: "基隆河左、右岸親水",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000840.jpg",
-  },
-  {
-    name: "大湖公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D727/E874/F732/1f40db60-ded0-416a-976e-baeea0464d8e.jpg",
-  },
-  {
-    name: "臺北忠烈祠",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000791.jpg",
-  },
-  {
-    name: "關渡碼頭-甘豆門",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000838.jpg",
-  },
-  {
-    name: "社子島環島與二重疏洪道自行車道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000846.jpg",
-  },
-  {
-    name: "七星山系-天母古道親山步道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000377.jpg",
-  },
-  {
-    name: "台北當代藝術館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000735.jpg",
-  },
-  {
-    name: "光點臺北",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000394.jpg",
-  },
-  {
-    name: "臺北市立美術館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D532/E822/F530/e168789d-2c35-4c18-af07-04185601e3da.jpg",
-  },
-  {
-    name: "行天宮北投分宮-忠義廟",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11002395.jpg",
-  },
-  {
-    name: "景美溪左、右岸自行車道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000844.jpg",
-  },
-  {
-    name: "國立歷史博物館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000725.jpg",
-  },
-  {
-    name: "冷水坑溫泉區",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000851.jpg",
-  },
-  {
-    name: "西門紅樓",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D237/E878/F721/4079be28-42bc-4153-aebd-6a1cbea84d5e.jpg",
-  },
-  {
-    name: "松山慈祐宮",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000935.jpg",
-  },
-  {
-    name: "關渡宮",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000948.jpg",
-  },
-  {
-    name: "北投文物館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D330/E107/F267/f51c663d-f50a-4691-9c6b-e578afea70d3.jpg",
-  },
-  {
-    name: "內溝溪景觀生態步道",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11003130.jpg",
-  },
-  {
-    name: "二二八和平公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000788.jpg",
-  },
-  {
-    name: "國立臺灣科學教育館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000729.jpg",
-  },
-  {
-    name: "指南宮",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000932.jpg",
-  },
-  {
-    name: "臺北啤酒工廠(原建國啤酒廠)",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000870.jpg",
-  },
-  {
-    name: "松山文創園區",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D26/E563/F587/cdeaa2fb-0f0e-4f4a-a078-170d23520800.jpg",
-  },
-  {
-    name: "大龍峒保安宮",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D775/E454/F120/05e7fcff-c925-4828-8e89-c0bfdd3bfe9c.JPG",
-  },
-  {
-    name: "雙溪生活水岸",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11002566.jpg",
-  },
-  {
-    name: "陽明山中山樓",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D370/E882/F626/420c6cf8-45c6-4c31-8e0a-42d44a1a5eac.jpg",
-  },
-  {
-    name: "臺北市鄉土教育中心(剝皮寮歷史街區)",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C1/D75/E624/F107/4192a9df-e1cd-49a5-900f-c392ae6a16f6.jpg",
-  },
-  {
-    name: "自來水博物館",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D346/E920/F1/050967e9-15b8-4ff5-89ca-ad4a4d6186a7.jpg",
-  },
-  {
-    name: "華中河濱公園",
-    image_url:
-      "https://www.travel.taipei/d_upload_ttn/sceneadmin/image/A0/B0/C0/D0/E197/F323/522aa425-345c-4ac4-96b8-b21a33f165ca.JPG",
-  },
-];
+fetch(
+  "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Data received:", JSON.stringify(data, null, 2)); // 打印完整的返回資料
 
-// 取得所有的 class 是 .image-item 元素（也就是 promotion1~3 和 Title1 ~ 10）
-const items = document.querySelectorAll(".image-item");
+    const attractions = data.data.results; // 獲取所有的景點資料
 
-// 遍歷資料並插入每一個項目
-data.forEach((item, index) => {
-  if (items[index]) {
-    // 取得每個項目的圖片和文字區域
-    const img = items[index].querySelector(".image"); // 將json的圖片塞到 class = image的地方 （也就是 promotion1~3 和 Title1 ~ 10）
-    const textDiv = items[index].querySelector(".text"); // 將json的文字塞到 class = text的地方 （也就是 promotion1~3 和 Title1 ~ 10）
+    // 取得所有的 .image-item 元素（也就是 promotion1~3 和 Title1 ~ 10）
+    const items = document.querySelectorAll(".image-item");
 
-    // 更新圖片的 src 屬性
-    img.src = item.image_url;
+    // 遍歷資料並插入每一個item
+    attractions.forEach((item, index) => {
+      if (items[index]) {
+        // 獲取每個項目的圖片和文字區域
+        const img = items[index].querySelector(".image"); // 獲取圖片區域
+        const textDiv = items[index].querySelector(".text"); // 獲取文字區域
 
-    // 更新文字內容
-    textDiv.textContent = item.name;
-  }
-});
+        // 處理 filelist 字串，將圖片 URL 提取出來
+        const imageUrls = item.filelist
+          .split("https://")
+          .filter((url) => url)
+          .map((url) => "https://" + url);
 
-// 獲取 Load More 按鈕和容器元素
-const loadMoreBtn = document.getElementById("load-more-btn");
-const container = document.querySelector("body"); // 可以選擇其他容器，這裡選擇 body
+        // 確保獲取到圖片 URL
+        if (img && imageUrls.length > 0) {
+          img.src = imageUrls[0]; // 使用第一個圖片 URL 更新圖片
+        } else {
+          console.error("Image URL not found for item", index); // 如果找不到圖片，則報錯
+        }
 
-// 目前顯示的資料索引，從上一輪 "Load More" 點擊後的最後索引開始
-let currentIndex = items.length;
+        // 更新文字內容
+        if (textDiv && item.stitle) {
+          textDiv.textContent = item.stitle; // 更新文字內容
+        } else {
+          console.error("Text not found for item", index); // 如果找不到文字，則報錯
+        }
+      }
+    });
 
-// 按下 Load More 按鈕時執行的函式
-loadMoreBtn.addEventListener("click", function () {
-  // 創建一個新的 big-and-mid-image-container 元素
-  const newContainer = document.createElement("div");
-  newContainer.classList.add("big-and-mid-image-container");
+    // 獲取 Load More 按鈕和容器元素
+    const loadMoreBtn = document.getElementById("load-more-btn");
+    const container = document.querySelector("body"); // 可以選擇其他容器，這裡選擇 body
 
-  // 設定新的項目數量（假設每次載入 10 個）
-  const newItems = data.slice(currentIndex, currentIndex + 10); // 從 data 中切取下一批資料
+    // 目前顯示的資料索引，從上一輪 "Load More" 點擊後的最後index開始
+    let currentIndex = items.length;
 
-  // 按照每 5 個顯示 5 個中圖和 3 個大圖來插入新內容
-  newItems.forEach((item, index) => {
-    const titleIndex = currentIndex + index + 1; // 計算標題的編號
+    // 按下 Load More 按鈕時執行的func
+    loadMoreBtn.addEventListener("click", function () {
+      // 創建一個新的 big-and-mid-image-container 元素
+      const newContainer = document.createElement("div");
+      newContainer.classList.add("big-and-mid-image-container");
 
-    let itemHTML = "";
-    // 根據索引決定是大圖還是中圖
-    if (index % 5 === 0) {
-      // index1與5是大圖
-      itemHTML = `
-          <div class="big-image-item image-item title-${titleIndex}">
-            <img src="${item.image_url}" alt="Title ${titleIndex}" class="big-image image" />
-            <div class="big-image-text text">${item.name}</div>
-          </div>
-    
-        `;
-    } else {
-      // 除了1與5整除的index以外是中圖
-      itemHTML = `
-          <div class="mid-image-item image-item title-${titleIndex}">
-            <img src="${item.image_url}" alt="Title ${titleIndex}" class="mid-image image" />
-            <div class="mid-image-text text">${item.name}</div>
-          </div>
-    
-        `;
-    }
+      // 設定新的item數量（假設每次載入 10 個）
+      const newItems = attractions.slice(currentIndex, currentIndex + 10); // 從 data 中切取下一批資料
 
-    // 將創建的 HTML 結構插入新的容器
-    newContainer.innerHTML += itemHTML;
-  });
+      // 按照每 5 個顯示 5 個中圖和 3 個大圖來插入新內容
+      newItems.forEach((item, index) => {
+        const titleIndex = currentIndex + index + 1; // 計算標題的編號
 
-  // 更新 currentIndex，確保下次點擊時是接續在已顯示的項目後面
-  currentIndex += 10;
+        let itemHTML = "";
+        // 根據索引決定是大圖還是中圖
+        const imageUrls = item.filelist
+          .split("https://")
+          .filter((url) => url)
+          .map((url) => "https://" + url);
+        const imageUrl = imageUrls.length > 0 ? imageUrls[0] : ""; // 取第一個圖片 URL
 
-  // 把新的容器元素插入到頁面的最後
-  container.appendChild(newContainer);
+        if (index % 5 === 0) {
+          // index1與5是大圖
+          itemHTML = `
+            <div class="big-image-item image-item title-${titleIndex}">
+              <img src="${imageUrl}" alt="Title ${titleIndex}" class="big-image image" />
+              <div class="big-image-text text">${item.stitle}</div>
+            </div>
+          `;
+        } else {
+          // 除了1與5整除的index以外是中圖
+          itemHTML = `
+            <div class="mid-image-item image-item title-${titleIndex}">
+              <img src="${imageUrl}" alt="Title ${titleIndex}" class="mid-image image" />
+              <div class="mid-image-text text">${item.stitle}</div>
+            </div>
+          `;
+        }
 
-  // 按下按鈕會自動滾動到頁面底部
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: "smooth",
-  });
-});
+        // 將創建的 HTML 結構插入新的容器
+        newContainer.innerHTML += itemHTML;
+      });
+
+      // 更新 currentIndex，確保下次點擊時是接續在已顯示的item後面
+      currentIndex += 10;
+
+      // 把新的容器元素插入到頁面的最後
+      container.appendChild(newContainer);
+
+      // 按下按鈕會自動滾動到頁面底部
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth",
+      });
+    });
+  })
+  .catch((error) => console.error("Error loading the JSON file:", error)); // 載入資料時出錯的話
